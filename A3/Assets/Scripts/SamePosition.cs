c48aa7601b3b6228d0e8ad3464e08545bf3542fa
Jeffherson Vega <jeffhersonlim@gmail.com>
2019-01-08 20:15:19 -0500

    Range Indicator Prototype In

---
+++ A3/Assets/Scripts/SamePosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamePosition : MonoBehaviour {
+
+	public GameObject host; // GameObject that this object copies the position of
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = host.transform.position;
+	}
+}
